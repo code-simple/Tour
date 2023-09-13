40090e9a65fca27a8b5e3f7882e2baa093c53835
code-simple <61619216+code-simple@users.noreply.github.com>
2023-09-13 19:52:56 +0500

    AppError Class errorController

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
-
+const AppError = require('./utils/appError');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
-
+const globalErrorHanlder = require('./controllers/errorController');
 const app = express();
 
 // Morgan Middlewares
@@ -26,11 +26,9 @@ app.use('/api/v1/users', userRouter);
 
 // Route Error handling
 app.all('*', (req, res, next) => {
-  res.status(404).json({
-    status: 'Failed',
-    message: `Cannot find route ${req.originalUrl} on this server`,
-  });
-  next();
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
 });
+
+app.use(globalErrorHanlder);
 
 module.exports = app;
