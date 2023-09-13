1436f274310c6bb6991b2cd8f620573e7780fa0b
code-simple <61619216+code-simple@users.noreply.github.com>
2023-09-13 19:10:10 +0500

    Route Error Handling

--- app.js
+++ app.js
@@ -24,4 +24,13 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
+// Route Error handling
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'Failed',
+    message: `Cannot find route ${req.originalUrl} on this server`,
+  });
+  next();
+});
+
 module.exports = app;
