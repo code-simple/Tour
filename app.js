66085bb8aaeb4d25ab79519e4bb4050d48f6987d
code-simple <imran1102@gmail.com>
2023-10-24 11:15:11 -0700

    CI CD

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan(':date[clf] ":method :url"'));
 }
 
+console.log('CI/CD');
 app.use(express.json());
 // Hosting files in public folder
 app.use(express.static(`${__dirname}/public`));
