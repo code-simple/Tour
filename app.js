702d3fbcf369e9bf7dc395df606c6417c8df2add
code-simple <imran1102@gmail.com>
2023-10-31 10:34:10 -0700

    express rate limiter

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 const AppError = require('./utils/appError');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const globalErrorHanlder = require('./controllers/errorController');
 
 const app = express();
-// Morgan Middlewares
+// Global Middlewares
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan(':date[clf] ":method :url"'));
 }
+
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests form this IP please try again in an hour',
+});
+
+app.use('/api', limiter);
 
 app.use(express.json());
 // Hosting files in public folder
